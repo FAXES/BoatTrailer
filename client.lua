bc7e9af50f9cd43af6a37e49425df0b0c75ccd2d
BuildTools <unconfigured@null.spigotmc.org>
2020-09-17 17:11:38 -0500

    Added the ability to add more trailers/vehicles easier.

--- client.lua
+++ client.lua
@@ -1,8 +1,3 @@
------------------------------------
---- Boat Trailer, Made by FAXES ---
------------------------------------
-
---- Config ---
 
 attachKey = 51 -- Index number for attach key - http://docs.fivem.net/game-references/controls/
 attachKeyName = "~INPUT_CONTEXT~" -- Key name (center column) of above key.
@@ -15,41 +10,67 @@ function GetVehicleInDirection(cFrom, cTo)
     return vehicle
 end
 
+vehicles = {
+    ["dinghy"] = {20, 0.0, -2.0, 0.08, 0.0, 0.0, 0.0, false, false, true, false, 20, true},
+    ["smallvehicle"] = {20, 0.0, -1.0, 0.45, 0.0, 0.0, 0.0, false, false, true, false, 20, true},
+    ["mediumvehicle"] = {20, 0.0, -1.0, 0.50, 0.0, 0.0, 0.0, false, false, true, false, 20, true},
+    ["predator"] = {20, 0.0, -1.6, 0.50, 0.0, 0.0, 0.0, false, false, true, false, 20, true},
+    ["nitrovehicle"] = {20, 0.0, -1.0, 0.10, 0.0, 0.0, 0.0, false, false, true, false, 20, true},
+    ["leo92"] = {10, 1.2, 2.2, 0.40, 0.0, 0.0, 0.0, false, false, true, false, 20, true},
+	["gatorciv"] = {10, 1.2, 2.2, 0.40, 0.0, 0.0, 0.0, false, false, true, false, 20, true},
+    ["snowmobile"] = {10, 1.2, 2.2, 0.40, 0.0, 0.0, 0.0, false, false, true, false, 20, true},
+    ["forklift"] = {10, 1.2, 2.2, 0.40, 0.0, 0.0, 0.0, false, false, true, false, 20, true},
+    ["forklift2"] = {10, 1.2, 2.2, 0.40, 0.0, 0.0, 0.0, false, false, true, false, 20, true}
+}
+
+trailers = {
+    ["vehicletrailer"] = true,
+    ["nbtrailer"] = true,
+    ["utvtrailer"] = true
+}
+
 Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(0)
-        local ped = GetPlayerPed(-1) -- Made the whole thing forgot to add this line lol, maybe thats why it broke #4:32AMLife
-        local veh = GetVehiclePedIsIn(ped)
-        if veh ~= nil then
-            if GetDisplayNameFromVehicleModel(GetEntityModel(veh)) == "DINGHY" then -- After a few hours here at 4am GetDisplayNameFromVehicleModel() got it working well :P
-                local belowFaxMachine = GetOffsetFromEntityInWorldCoords(veh, 1.0, 0.0, -1.0)
-				local boatCoordsInWorldLol = GetEntityCoords(veh)
-                local trailerLoc = GetVehicleInDirection(boatCoordsInWorldLol, belowFaxMachine)
-                
-				if GetDisplayNameFromVehicleModel(GetEntityModel(trailerLoc)) == "BOATTRAILER" then -- Is there a trailer????
-                    if IsEntityAttached(veh) then -- Is boat already attached?
-                        if IsControlJustReleased(1, attachKey) then -- detach
+	while true do
+		Citizen.Wait(10)
+		local ped = GetPlayerPed(-1)
+		local veh = GetVehiclePedIsIn(ped)
+		if veh ~= nil then
+			local vehName = string.lower(GetDisplayNameFromVehicleModel(GetEntityModel(veh)))
+			if vehicles[vehName] then
+				local belowFaxMachine = GetOffsetFromEntityInWorldCoords(veh, 1.0, 6.0, -5.0)
+				local vehicleCoordsInWorldLol = GetEntityCoords(veh)
+                local trailerLoc = GetVehicleInDirection(vehicleCoordsInWorldLol, belowFaxMachine)
+				local trailName = string.lower(GetDisplayNameFromVehicleModel(GetEntityModel(trailerLoc)))
+                if trailers[trailName] then
+					if IsEntityAttached(veh) then
+						if IsControlJustReleased(1, attachKey) then
 							DetachEntity(veh, false, true)
 						end
-                        -- Start Prompt
-                        Citizen.InvokeNative(0x8509B634FBE7DA11, "STRING") -- BeginTextCommandDisplayHelp()
-						Citizen.InvokeNative(0x5F68520888E69014, "Press " .. attachKeyName .. " to detach boat.") -- AddTextComponentScaleform()
+						-- Start Prompt
+						Citizen.InvokeNative(0x8509B634FBE7DA11, "STRING") -- BeginTextCommandDisplayHelp()
+						Citizen.InvokeNative(0x5F68520888E69014, "Press " .. attachKeyName .. " to detach entity.") -- AddTextComponentScaleform()
 						Citizen.InvokeNative(0x238FFE5C7B0498A6, 0, false, true, -1) -- EndTextCommandDisplayHelp()
-                    else
-                        if IsControlJustReleased(1, attachKey) then -- Attach
-							AttachEntityToEntity(veh, trailerLoc, 20, 0.0, -1.0, 0.25, 0.0, 0.0, 0.0, false, false, true, false, 20, true)
-							TaskLeaveVehicle(ped, veh, 64)
-                        end
+					else
+                        if IsControlJustReleased(1, attachKey) then
+                            local vehicle = vehicles[vehName]
+                            if vehName == "nitrovehicle" then
+                                if trailName == "nbtrailer" then
+                                    vehicle = {2, 0.18, -1.0, 0.20, 0.0, 0.0, 0.0, false, false, true, false, 20, true}
+                                end
+                            elseif vehName == "mediumvehicle" then
+                                if trailName == "nbtrailer" then
+                                     vehicle = {2, 0.18, -1.0, 0.65, 0.0, 0.0, 0.0, false, false, true, false, 20, true}
+                                end
+                            end
+							AttachEntityToEntity(veh, trailerLoc, vehicle[1], vehicle[2], vehicle[3], vehicle[4], vehicle[5], vehicle[6], vehicle[7], vehicle[8], vehicle[9], vehicle[10], vehicle[11], vehicle[12], vehicle[13])
+						end
                         -- Start Prompt
-                        Citizen.InvokeNative(0x8509B634FBE7DA11, "STRING") -- BeginTextCommandDisplayHelp()
-						Citizen.InvokeNative(0x5F68520888E69014, "Press " .. attachKeyName .. " to attach boat.") -- AddTextComponentScaleform()
+						Citizen.InvokeNative(0x8509B634FBE7DA11, "STRING") -- BeginTextCommandDisplayHelp()
+						Citizen.InvokeNative(0x5F68520888E69014, "Press " .. attachKeyName .. " to attach entity.") -- AddTextComponentScaleform()
 						Citizen.InvokeNative(0x238FFE5C7B0498A6, 0, false, true, -1) -- EndTextCommandDisplayHelp()
-                        -- Made by Faxes with some help of the bois
 					end
-                end
-            end
-        end
+				end
+			end
+		end
     end
-    -- Just a comment here. Why the fuck not? Its 6 am now
 end)
--- All done, only lots of bullshit with code lol. Timestamp, its not 6:15AM. Why we still here? Just to suffer?
